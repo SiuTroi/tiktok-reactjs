e7c52c94956c7bd57ca95521e4d774b512a39589
SiuTroi <siutroi004@gmail.com>
2023-01-30 18:57:48 +0700

    header ui #6

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -10,7 +10,7 @@ function AccountItem() {
     <div className={cx('wrapper')}>
       <img
         className={cx('avatar')}
-        src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/f359ad9b23ffbeff595ff074fa6082b2~c5_300x300.webp?x-expires=1675044000&x-signature=FLhOmirxcYXSPR21ARtzXKNgdWQ%3D"
+        src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/f359ad9b23ffbeff595ff074fa6082b2~c5_100x100.jpeg?x-expires=1675159200&x-signature=gujNHTnL8m6VUULoh45KRNPjbDg%3D"
         alt="Criss"
       />
       <div className={cx('info')}>
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -4,13 +4,20 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faCircleQuestion,
   faCircleXmark,
+  faCloudUpload,
+  faCoins,
   faEarthAsia,
   faEllipsisVertical,
+  faGear,
   faKeyboard,
   faMagnifyingGlass,
+  faSignOut,
   faSpinner,
+  faUser,
 } from '@fortawesome/free-solid-svg-icons';
-import Tippy from '@tippyjs/react/headless';
+import Tippy from '@tippyjs/react';
+import HeadlessTippy from '@tippyjs/react/headless';
+import 'tippy.js/dist/tippy.css';
 
 import Button from '~/components/Button';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
@@ -50,8 +57,35 @@ const MENU_ITEMS = [
     title: 'Keyboard shortcuts',
   },
 ];
+
+const userMenu = [
+  {
+    icon: <FontAwesomeIcon icon={faUser} />,
+    title: 'View Profile',
+    to: '/@troi',
+  },
+  {
+    icon: <FontAwesomeIcon icon={faCoins} />,
+    title: 'Get Coins',
+    to: '/coin',
+  },
+  {
+    icon: <FontAwesomeIcon icon={faGear} />,
+    title: 'Settings',
+    to: '/settings',
+  },
+  ...MENU_ITEMS,
+  {
+    icon: <FontAwesomeIcon icon={faSignOut} />,
+    title: 'Log out',
+    to: '/logout',
+    separate: true,
+  },
+];
 function Header() {
   const [searchResult, setSearchResult] = useState([]);
+
+  const currentUser = true;
 
   useEffect(() => {
     setTimeout(() => {
@@ -76,7 +110,7 @@ function Header() {
         {/* Logo */}
         <img src={images.logo} alt="tiktok logo" />
         {/* Search */}
-        <Tippy
+        <HeadlessTippy
           interactive
           visible={searchResult.length > 0}
           render={(attrs) => (
@@ -99,22 +133,40 @@ function Header() {
             </button>
             {/* search icon btn */}
             <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
-            <Tippy content="Tìm kiếm" placement="bottom">
-              <button className={cx('search-btn')}>
-                <FontAwesomeIcon icon={faMagnifyingGlass} />
-              </button>
-            </Tippy>
-          </div>
-        </Tippy>
-        {/* Action */}
-        <div className={cx('action')}>
-          <Button text>Upload</Button>
-          <Button primary>Log in</Button>
-
-          <Menu items={MENU_ITEMS} onChange={handleMenuChange}>
-            <button className={cx('more-btn')}>
-              <FontAwesomeIcon icon={faEllipsisVertical} />
+            <button className={cx('search-btn')}>
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
             </button>
+          </div>
+        </HeadlessTippy>
+        {/* Action */}
+        <div className={cx('actions')}>
+          {currentUser ? (
+            <>
+              <Tippy delay={[0, 100]} content="Upload Video" placement="bottom">
+                <button className={cx('action-btn')}>
+                  <FontAwesomeIcon icon={faCloudUpload} />
+                </button>
+              </Tippy>
+            </>
+          ) : (
+            <>
+              <Button text>Upload</Button>
+              <Button primary>Log in</Button>
+            </>
+          )}
+
+          <Menu items={currentUser ? userMenu : MENU_ITEMS} onChange={handleMenuChange}>
+            {currentUser ? (
+              <img
+                className={cx('user-avatar')}
+                src="https://scontent.fdad3-1.fna.fbcdn.net/v/t39.30808-1/326347525_572863108029293_5025819455142916048_n.jpg?stp=dst-jpg_p200x200&_nc_cat=108&ccb=1-7&_nc_sid=7206a8&_nc_ohc=AgllMtbLFlMAX_A2K4y&_nc_ht=scontent.fdad3-1.fna&oh=00_AfCpUCQkWdkQblYzs1g18IVB8NwIanGsDrEsbad7qi5CrA&oe=63DD2646"
+                alt="Siu troi"
+              />
+            ) : (
+              <button className={cx('more-btn')}>
+                <FontAwesomeIcon icon={faEllipsisVertical} />
+              </button>
+            )}
           </Menu>
         </div>
       </div>
